8e1e83aeac8ec35753c265473f925f63898c15bf
SlimJUC <63930423+SlimJUC@users.noreply.github.com>
2020-05-09 02:54:38 +0300

    Update game.py

--- game.py
+++ game.py
@@ -16,31 +16,47 @@ print("""
         """ )
 
 cont = input("Press enter to continue....")
+print(" ================================ ")
+user_credit = int(input("Buy In Credit: "))
+print(" ================================== ")
 
 def Roulette():
-    user_num = int(input("Enter a number from 0 to 36: "))
 
-    lucky_num = randint(0, 36)
 
-    if user_num == lucky_num:
-        print("Congratulations, You guessed the correct numer! ")
+    global user_credit
+    if user_credit <= 0:
+        print(" You do not have enough credits to play ")
     else:
-        print ("Sorry, its not your lucky day. The lucky number was " + str(lucky_num))
+        
+       user_num = int(input("Enter a number from 0 to 36: "))
 
+       lucky_num = randint(0, 36)
 
+       if user_num == lucky_num:
+        print("Congratulations, You guessed the correct numer! ")
+       else:
+           print ("Sorry, its not your lucky day. The lucky number was " + str(lucky_num))
+           
+
+    print ("====================================")
+    again = input("Wanna try again? yes or no: ")
+    for i in again:
+        if again == "yes":
+            user_credit-=1
+            print ("Player Remaining Credit = " + str(user_credit))
+
+            Roulette()
+        elif again == "no":
+            print("Thank you for playing...")
+        else:
+            print("write 'yes' or 'no' no CAPS")   
 
 Roulette()
 print("============================================")
 
 
 
-again = input("Wanna try again? yes or no: ")
+    
 
-for i in again:
-    if again == "yes":
-        Roulette()
-    elif again == "no":
-        print("Thank you for playing...")
-    else:
-        print("write 'yes' or 'no' no CAPS")    
+ 
 
