344f48b8b99745bf2f204311fd411ef93ef16748
Salim J <63930423+SlimJUC@users.noreply.github.com>
2023-02-16 13:19:24 +0300

    General Improvements
    Here's a summary of the changes that were made:
    
    The code was refactored into a class called Roulette, which has methods for starting the game, buying credits, playing a round, and displaying the final results.
    
    The buy_credits() method was updated to include input validation to make sure that the user enters a valid number for the buy-in credit.
    
    The play_round() method was updated to include input validation to make sure that the user enters a valid number for the guessed number.
    
    The code was updated to limit the number of tries to 10 and to display the remaining tries after each round.
    
    The code was updated to display the winning number after each round.
    
    The code was updated to handle errors that may occur during execution, such as invalid input or running out of credits.

--- game.py
+++ game.py
@@ -1,60 +1,70 @@
 from random import randint
-    
-print("============================================")
 
-print("""        
+class Roulette:
+    def __init__(self):
+        self.user_credit = 0
+        self.max_tries = 10
+        self.current_try = 0
+
+    def start(self):
+        print("============================================")
+        print("""        
         Welcome to single number Roulette
 
         Each try costs 1 credit from the BUY-IN credit balance
         
-        If you hit the correct number you will be credited + 36 credits
-=============================================
-
-        """ )
-
-cont = input("Press enter to start the game....") 
-print(" ================================ ")
-user_credit = int(input("Buy In Credit: "))
-print(" ================================== ")
-
-def Roulette():
-
-
-    global user_credit
-    if user_credit <= 0:
-        print(" You do not have enough credits to play ")
-        exit()
-    else:
+        If you hit the correct number you will be credited +    36 credits
+        =============================================""")
         
-       user_num = int(input("Enter a number from 0 to 36: "))
+        self.user_credit = self.buy_credits()
 
-       lucky_num = randint(0, 36)
-
-       if user_num == lucky_num:
-        user_credit += 36   
-        print("""Congratulations, You guessed the correct numer!
+        while self.current_try < self.max_tries and self.user_credit > 0:
+            self.play_round()
+            self.current_try += 1
         
-        You have been credited  36 credits
+        print("============================================")
+        print("Game Over! Thank you for playing!")
+        print("Total Credit Available Now is:", self.user_credit)
+        print("============================================")
+
+    def buy_credits(self):
+        while True:
+            try:
+                user_credit = int(input("Buy In Credit (min 10 credits): "))
+                if user_credit < 10:
+                    print("Minimum Buy In Credit is 10")
+                else:
+                    break
+            except ValueError:
+                print("Invalid input, please enter a valid number")
+        print("============================================")
+        return user_credit
+
+    def play_round(self):
+        print("Remaining Tries:", self.max_tries - self.current_try)
+        print("Player Remaining Credit:", self.user_credit)
+        while True:
+            try:
+                user_num = int(input("Enter a number from 0 to 36: "))
+                if user_num < 0 or user_num > 36:
+                    raise ValueError("Invalid input, please enter a number between 0 and 36")
+                break
+            except ValueError as e:
+                print(e)
         
-        Total Credit Available Now is: """ +str(user_credit))
+        lucky_num = randint(0, 36)
+        print("The lucky number is:", lucky_num)
 
-       else:
-           print ("Sorry, its not your lucky day. The lucky number was " + str(lucky_num))
-           
-
-    print ("====================================")
-    again = input("Wanna try again? yes or no: ")
-    for i in again:
-        if again == "yes":
-            user_credit-=1
-            print ("Player Remaining Credit = " + str(user_credit))
-
-            Roulette()
-        elif again == "no":
-            print("Thank you for playing...")
-            exit()
+        if user_num == lucky_num:
+            self.user_credit += 36
+            print("""Congratulations, You guessed the correct number!
+            You have been credited 36 credits
+            Total Credit Available Now is: """ +str(self.user_credit))
         else:
-            print("write 'yes' or 'no' no CAPS")   
+            self.user_credit -= 1
+            print("Sorry, it's not your lucky day. You lost 1 credit.")
+        
+        print("============================================")
 
-Roulette()
-print("============================================")
+roulette = Roulette()
+roulette.start()
